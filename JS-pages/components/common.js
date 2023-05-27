eaacba5cba663adaa479c02bcf5ef8fd7932b16b
Jason <jason999220@gmail.com>
2023-05-27 17:38:25 +0800

    js array finish , nodeList 太複雜

--- JS-pages/components/common.js
+++ JS-pages/components/common.js
@@ -7,13 +7,6 @@ const typeFinish = document.getElementById("context-span"); // js result context
 const jsCode = document.querySelectorAll(".js-code");
 const consoleIframe = document.querySelectorAll(".console-iframe");
 
-// 可以移除
-for (let i = 0; i < handleOutput.length; i++) {
-  handleOutput[i].addEventListener("click", () => {
-    jsResultText[i].style.opacity = 1;
-  });
-}
-
 // 第三版
 for (let i = 0; i < jsCodeRun.length; i++) {
   jsCodeRun[i].addEventListener("click", () => {
@@ -21,11 +14,20 @@ for (let i = 0; i < jsCodeRun.length; i++) {
     // resultIframe[i].contentWindow.eval(jsCode[i].value); //  save JS code
     try {
       // 其餘參數 ...args => 不確定參數會有幾個的情況
+      // console.log = (msg, ...args) => {
+      //   consoleIframe[
+      //     i
+      //   ].contentDocument.body.innerHTML += ` <p style="border-bottom: 1px solid #fff;  font-size: 1.3rem;padding-bottom: 0.5rem">
+      //       ${msg} ${args}
+      //     </p>`; // 在<iframe>中顯示結果
+      // };
+
+      // 使用 JSON.stringify 將全部資料已字串形式存取不然用上面那種方式陣列會消失，但麻煩是變數字串也會直接被印出來
       console.log = (msg, ...args) => {
         consoleIframe[
           i
         ].contentDocument.body.innerHTML += ` <p style="border-bottom: 1px solid #fff;  font-size: 1.3rem;padding-bottom: 0.5rem">
-            ${msg} ${args}
+            ${JSON.stringify(msg)}  ${args}
           </p>`; // 在<iframe>中顯示結果
       };
       eval(jsCode[i].value); // 執行當前JS代碼
